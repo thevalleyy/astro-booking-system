168387cf3a1226acc99bd51b0357b59641e7d699
thevalleyy <74456686+thevalleyy@users.noreply.github.com>
2024-12-01 18:39:34 +0100

    create generateTable.js

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 ```json
 {
     "settings": {
-        "port": 3000
+        "port": 3000,
+        "start": "18:00",
+        "end": "21:00",
+        "increment": "00:20"
     },
     "log": {
         "enabled": false,
@@ -11,6 +14,14 @@
         "requests": true,
         "log_path": "./logs/",
         "log_extension": ".log"
+    },
+    "html-meta-data": {
+        "title": "Astro-Booking-System",
+        "description": "Buchen Sie Ihren Termin!",
+        "url": "https://astro.rathenau.eu/",
+        "image": "https://apod.nasa.gov/apod/image/1611/PacmanCrawfordNew2048.jpg",
+        "large_image": true,
+        "color": "#000181"
     }
 }
 ```
--- js/generateTable.js
+++ js/generateTable.js
@@ -1,12 +1,32 @@
 const generateTableHeaders = require("./tableHeaders.js");
+const fs = require("node:fs");
 
+/**
+ * Generates a json table at data/table.json based on the config file
+ */
 function generateTable() {
     const config = require("../config.json");
-
     const headers = generateTableHeaders(config.settings.start, config.settings.end, config.settings.increment);
-    console.log(headers);
-}
 
-generateTable();
+    const table = {
+        updated: new Date().getTime(),
+        data: {},
+    };
+
+    for (let i = 0; i < headers.length; i++) {
+        table.data[headers[i]] = { 0: config.settings.default };
+    }
+
+    fs.access("../data/table.json", fs.constants.F_OK, (err) => {
+        // don't overwrite the file if it exists
+        if (err) {
+            try {
+                fs.mkdirSync("../data").catch();
+            } catch (e) {} // the directory already exists, so ignore the error
+
+            fs.writeFileSync("../data/table.json", JSON.stringify(table, null, 4));
+        }
+    });
+}
 
 module.exports = generateTable;

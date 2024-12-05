3c0faf2ad2af90b1d54f4ca7177249e5911f45ae
Dodorex <dodorex@protonmail.com>
2024-12-05 22:48:35 +0100

    sends confirmation email
    Server sends confirmation email if all checks passed and the slots are booked. E-mail contains data about ammount of slots and selected time frame. Uses "nodemailer" module

--- js/api/addEntry.js
+++ js/api/addEntry.js
@@ -4,6 +4,7 @@ const keys = Object.keys(require("../../config.json")["settings"]["default"]);
 const checks = require("../../config.json")["settings"]["checks"];
 const slotsPerColumn = require("../../config.json")["settings"]["slotsPerColumn"];
 const validSlots = Object.keys(require("../../data/table.json")["data"]);
+const confirmationEmailSettings = require("../../config.json")["settings"]["confirmationEmail"];
 
 /**
  * Adds a new entry to the json file
@@ -94,7 +95,7 @@ function addEntry(query) {
         };
     }
 
-    // TODO: check if the user has already booked max slots
+    // // TODO: check if the user has already booked max slots
     // TODO: mail confirmation
     // TODO: Admin panel
     // TODO: visible error if ws fails
@@ -130,6 +131,39 @@ function addEntry(query) {
         // backup file before writing
         fs.copyFileSync("./data/table.json", `./data/backup/table_${Date.now()}.json`);
         fs.writeFileSync("./data/table.json", JSON.stringify({ updated: Date.now(), data: data }, null, 4));
+
+        //Send confirmation E-Mail to user
+        let nodemailer = reqire('nodemailer')
+        let transporter = nodemailer.createTransport({
+            service: confirmationEmailSettings.service,
+            auth: {
+                user: confirmationEmailSettings.serverEmailAddress,
+                pass: confirmationEmailSettings.serverEmailPassword
+            }
+        });
+        
+        var confirmationEmailOptions = {
+            from: confirmationEmailSettings.serverEmailAddress,
+            to: query["email"],
+            subject: `Confirmation for booking ${Number(query["bookedSlots"])} Slots for the Astro dingens`,
+            text: `Hallo ${query["firstname"]}, \n\nes freut uns, dass du ${Number(query["bookedSlots"])} Slots um ${query["timeSlot"]} gebucht hast und wünschen dir viel Spaß. :)\nMit freundlichen Grüßen,\nDas Astroteam`
+        };
+
+        transporter.sendMail(confirmationEmailOptions, function(error, info) {
+            if (error) {
+                console.error(error);
+                return {
+                    code: 500,
+                    success: false,
+                    message: "confirmation E-main couldn't be sent. Maybe invalid email? Maybe check config file?"
+                }
+            } else {
+                console.log('Email sent: ' + info.response);
+            }
+        });
+        
+
+
     } catch (error) {
         console.error(error);
         return {

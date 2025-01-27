837fda97386bc7427544984b8d66138c9e6dd6f7
thevalleyy <74456686+thevalleyy@users.noreply.github.com>
2025-01-27 15:48:55 +0100

    Enforced mutual lock

--- pages/api/deleteAll.js
+++ pages/api/deleteAll.js
@@ -1,10 +1,31 @@
 import deleteAll from "../../js/api/deleteAll.js";
 import request from "../../js/request.js";
 
+let isLocked = false;
+
+async function acquireLock() {
+    isLocked = true;
+}
+
+function releaseLock() {
+    isLocked = false;
+}
+
 export default async function handler(req, res) {
     request(req);
+
+    // check if locked
+    if (isLocked) {
+        res.status(503).json({ code: 503, message: "Another admin is performing this action right now", success: false });
+    }
+
+    // acquire lock
+    acquireLock();
 
     // pass elemtents to the function
     const result = await deleteAll(req, JSON.parse(JSON.stringify(req.body)));
     res.status(result.code).json(result);
+
+    // release lock
+    releaseLock();
 }

3b8abeb1e3b642a621a85467d684351843c209c6
thevalleyy <74456686+thevalleyy@users.noreply.github.com>
2025-03-31 18:56:20 +0200

    reset.js übersetzt

--- js/reset.js
+++ js/reset.js
@@ -3,9 +3,9 @@ import promptSync from "prompt-sync";
 
 const prompt = promptSync({ sigint: true });
 
-const consent = prompt("\x1b[41mDo you agree to reset the data? (type 'yes' to confirm):\x1b[0m ").toLowerCase() == "yes";
+const consent = prompt("\x1b[41mAlle Daten zurücksetzen? ('ja' zur Bestätigung eingeben):\x1b[0m ").toLowerCase() == "ja";
 if (!consent) {
-    console.log("\x1b[42mReset aborted\x1b[0m");
+    console.log("\x1b[42mReset abgebrochen\x1b[0m");
     process.exit();
 }
 
@@ -21,4 +21,4 @@ try {
     fs.rmSync("./data/table.json");
 } catch (error) {} // already removed
 
-console.log("\x1b[41mData reset successful\x1b[0m");
+console.log("\x1b[41mDaten erfolgreich zurückgesetzt\x1b[0m");
